74bffaaf776cb0f6d124b7b8f493250b02601100
Tushardhingra6281 <108275929+Tushardhingra6281@users.noreply.github.com>
2025-07-04 06:13:58 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all js as new version 
+
+// alert(3 + 3)   we are using node js , not browser
+
+let name = "tushar"
+let age = 21
+let isloggedIn = true
+
+// number 2 to power 53 
+// bigint
+// string => "" 
+// boolean => true / false
+// null =>standlone value 
+// undefined =>
+// symbol 
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
+
+
+
