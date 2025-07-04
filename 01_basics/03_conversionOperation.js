770a53bb1c2578dce66a642f3324d3d8fc560925
Tushardhingra6281 <108275929+Tushardhingra6281@users.noreply.github.com>
2025-07-04 06:38:24 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "54ab"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let vauleInNumber = Number(score)
+console.log(typeof vauleInNumber);
+console.log(vauleInNumber);
+
+
+// "33" => 33
+// "33ab" => Nan
+// true => 1; false = 0
+
+let isLoggedIn = "tushar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 =>false
+// "" => false
+// "tushar" =>true
+
+let number = "34"
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
