e86ef9c09ad5a1a38f242c914821780aeb0adec0
Tushardhingra6281 <108275929+Tushardhingra6281@users.noreply.github.com>
2025-07-06 16:05:39 +0000

    Math and Num

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,52 @@
+// console.log("dj project");
+// let name = "tushar"
+// let roll_no = 2221112;
+// console.log(`hello my name is ${name} amd my roll no is ${roll_no}`);
+
+
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  // decimal value show krega number ke baad jese iska output aayega 100.00
+
+
+const otherValue = 12.58
+// console.log(otherValue.toPrecision(3));  //iska mtlb ye h decimal se pehle agr 3 value h to vo round off krke de dega jese agr 12.58 h iska ans aayega 12.6 or agr value h 123.58 to iska ans aayega 124 value precise krke de rha h 
+
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-in'));  //toLocaleString ka mtlb h comma lga ke de dega number me 
+
+
+// +++++++++++++++++++++++  Maths ++++++++++++++++++++++
+
+// console.log(Math.sqrt(12));
+
+// console.log(Math.abs(-12));  // negative value ko positive me krta h  
+
+// console.log(Math.round(12.5));   // round off value deta h 
+
+// console.log(Math.ceil(13.2));  // jra se bhi 12 se jada hua to agle number aayega output me iska ans aayega 14
+
+// console.log(Math.floor(10.1));  // isme ans lowest value uthayega hmesha jese iska ans aayega 10
+
+
+console.log(Math.random());  // 0 or 1 ke beech me value aayegi iski hmesha 
+
+console.log((Math.random()*10 ) + 1); 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)+ min));
+
+
+
+
+
+
+
+ 
