8561a7e1ed30ba2b34dd69f9ae81360f61ccf559
Tushardhingra6281 <108275929+Tushardhingra6281@users.noreply.github.com>
2025-07-10 05:02:50 +0000

    Function part 1 & 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,8 +37,42 @@ function loginUserMessage(username = "tushar"){
      return `${username} just logged in`
 
 }
-console.log(loginUserMessage())  // agr hum kuch pass hi nhi krege to undefned aayega likha 
+// console.log(loginUserMessage())  // agr hum kuch pass hi nhi krege to undefned aayega likha 
 
+
+function CalculateCartPrice(val1 , val2 ,...num1){   // ... isko rest operator bhi bola jata h or spread operator bhi 
+    return num1;
+}
+
+// console.log(CalculateCartPrice(200,300,400,600,700));  // 200 or 300 val1 or val2 me chli gyi h 
+
+
+const user = {
+    name : "tushar",
+    price : 200
+}
+
+
+function handleObject(anyobject){
+    console.log(`USername is ${anyobject.name} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({                    //dono method use kr skte h hmari marzi h hmne kya use krna h  
+    name : "mistri",
+    price : 420
+});
+
+
+const myNewArray =[200,400,600,800]
+
+function returnSecondvalue(getarray){
+    return getarray[1]
+}
+
+console.log(returnSecondvalue(myNewArray));
 
 
 
